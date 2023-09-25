e9f83a32313454daac0d6ece20e4c7ced35eaaac
IlyaPopov64 <144610544+IlyaPopov64@users.noreply.github.com>
2023-09-25 10:02:50 +0700

    Laba2.2
    Laba2.2

---
+++ Laba2.2/Laba2.2/Laba2.2.cpp
@@ -0,0 +1,14 @@
+﻿
+#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int n, a;
+	cout << "Введите трехзначное число n:" << endl;
+	cin >> n;
+	a = n % 100;
+	cout << "Результат вычислений a:" << endl;
+	cout << a << endl;
+	return 0;
+}
